e7ab316f94389911030a47fc44a944781ef22478
Benjamin Clark <clarkben@fb.com>
2021-10-19 15:27:16 -0400

    Add platform creation code.

--- src/scripts/MainScene.js
+++ src/scripts/MainScene.js
@@ -1,10 +1,12 @@
 import * as PIXI from "pixi.js";
 import { Globals } from "./Globals";
 import { Background } from "./Background";
+import { Platforms } from "./Platforms";
 export class MainScene {
     constructor() {
         this.container = new PIXI.Container();
         this.createBackground();
+        this.createPlatforms();
     }
 
     createBackground() {
@@ -12,7 +14,12 @@ export class MainScene {
         this.container.addChild(this.bg.container);
     }
 
+    createPlatforms() {
+        this.platforms = new Platforms();
+        this.container.addChild(this.platforms.container);
+    }
+
     update(dt) {
-        this.bg.update(dt); 
+        this.bg.update(dt);
     }
 }
